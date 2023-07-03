505b32803da3e9ae7984c1c0c6ab8af8f7a815c4
ICSF2442 <icsf2442@gmail.com>
2023-07-03 19:32:30 +0100

    todas as classes adaptadas para o uso da base de dados

--- src/main/java/comn/objects/Apartamento.java
+++ src/main/java/comn/objects/Apartamento.java
@@ -1,6 +1,13 @@
 package comn.objects;
 
+import comn.functions.Database;
 import comn.interfaces.slogan;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Apartamento extends Acomodacao implements slogan {
 
     private Integer id;
@@ -15,13 +22,204 @@ public class Apartamento extends Acomodacao implements slogan {
 
     private int cozinha;
 
+    public Apartamento(Integer id) throws SQLException, IOException {
+        if (id != null && Database.getConnection() != null) {
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM apartamento WHERE id = " + id);
+
+                if (resultSet.next()) {
+                    this.id = resultSet.getInt("id");
+                    this.nQuartos = resultSet.getInt("nQuartos");
+                    this.areaTotal = resultSet.getDouble("areaTotal");
+                    this.nCasaDeBanhos = resultSet.getInt("nCasaDeBanhos");
+                    this.internet = resultSet.getInt("internet");
+                    this.cozinha = resultSet.getInt("cozinha");
+                }
+
+                resultSet.close();
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Object[] toArray() {
+        Object[] array = new Object[6];
+        array[0] = this.id;
+        array[1] = this.nQuartos;
+        array[2] = this.areaTotal;
+        array[3] = this.nCasaDeBanhos;
+        array[4] = this.internet;
+        array[5] = this.cozinha;
+        return array;
+    }
+
+    public void store() {
+        String[] fields = {"id", "nQuartos", "areaTotal", "nCasaDeBanhos", "internet", "cozinha"};
+
+        if (this.id == null) {
+            this.id = Database.getNextIncrement("apartamento");
+
+            StringBuilder columns = new StringBuilder();
+            StringBuilder values = new StringBuilder();
+            for (String field : fields) {
+                columns.append(", ").append(field);
+                values.append(", ").append(this.getFieldValue(field) != null ? "'" + this.getFieldValue(field) + "'" : "NULL");
+            }
+
+            columns = new StringBuilder(columns.substring(2));
+            values = new StringBuilder(values.substring(2));
+            String sql = "INSERT INTO apartamento (" + columns + ") VALUES (" + values + ");";
+
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                statement.executeUpdate(sql);
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            StringBuilder values = new StringBuilder();
+            String sql = "UPDATE apartamento SET ";
+            for (String field : fields) {
+                values.append(",").append(field).append(" = ").append(this.getFieldValue(field) != null ? "'" + this.getFieldValue(field) + "'" : "NULL");
+            }
+
+            values = new StringBuilder(values.substring(1));
+            sql = "UPDATE apartamento SET " + values + " WHERE id = " + this.id;
+
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                statement.executeUpdate(sql);
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Apartamento[] search(Integer id, Integer nQuartos, Double areaTotal, Integer nCasaDeBanhos, Integer internet, Integer cozinha) {
+        String sql = "SELECT id FROM apartamento WHERE 1=1";
+
+        if (id != null) {
+            sql += " AND (id = " + id + ")";
+        }
+        if (nQuartos != null) {
+            sql += " AND (nQuartos = " + nQuartos + ")";
+        }
+        if (areaTotal != null) {
+            sql += " AND (areaTotal = " + areaTotal + ")";
+        }
+        if (nCasaDeBanhos != null) {
+            sql += " AND (nCasaDeBanhos = " + nCasaDeBanhos + ")";
+        }
+        if (internet != null) {
+            sql += " AND (internet = " + internet + ")";
+        }
+        if (cozinha != null) {
+            sql += " AND (cozinha = " + cozinha + ")";
+        }
+
+        Apartamento[] ret = null;
+
+        try {
+            Statement statement = Database.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            resultSet.last();
+            int rowCount = resultSet.getRow();
+            resultSet.beforeFirst();
+
+            if (rowCount > 0) {
+                ret = new Apartamento[rowCount];
+                int index = 0;
+
+                while (resultSet.next()) {
+                    int apartamentoId = resultSet.getInt("id");
+                    ret[index] = new Apartamento(apartamentoId);
+                    index++;
+                }
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    public void remove() {
+        if (this.id != null) {
+            String sql = "DELETE FROM apartamento WHERE id = " + this.id;
+
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                statement.executeUpdate(sql);
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static int find(Integer id, Integer nQuartos, Double areaTotal, Integer nCasaDeBanhos, Integer internet, Integer cozinha) {
+        String sql = "SELECT id FROM apartamento WHERE 1=1";
+
+        if (id != null) {
+            sql += " AND (id = " + id + ")";
+        }
+        if (nQuartos != null) {
+            sql += " AND (nQuartos = " + nQuartos + ")";
+        }
+        if (areaTotal != null) {
+            sql += " AND (areaTotal = " + areaTotal + ")";
+        }
+        if (nCasaDeBanhos != null) {
+            sql += " AND (nCasaDeBanhos = " + nCasaDeBanhos + ")";
+        }
+        if (internet != null) {
+            sql += " AND (internet = " + internet + ")";
+        }
+        if (cozinha != null) {
+            sql += " AND (cozinha = " + cozinha + ")";
+        }
+
+        try {
+            Statement statement = Database.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            int count = resultSet.next() ? 1 : 0;
+            resultSet.close();
+            statement.close();
+            return count;
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    private Object getFieldValue(String fieldName) {
+        return switch (fieldName) {
+            case "id" -> this.id;
+            case "nQuartos" -> this.nQuartos;
+            case "areaTotal" -> this.areaTotal;
+            case "nCasaDeBanhos" -> this.nCasaDeBanhos;
+            case "internet" -> this.internet;
+            case "cozinha" -> this.cozinha;
+            default -> null;
+        };
+    }
+
     //conceito de polimorfismo
     public String descricao() {
         return"Apartamento";
     }
 
 
-    //utilização do slogan
+    //utilização do ‘slogan’
     @Override
     public String slogan(){
         return "Bem vindo á reserva de Apartamentos!";
--- src/main/java/comn/objects/HotelResort.java
+++ src/main/java/comn/objects/HotelResort.java
@@ -1,11 +1,100 @@
 package comn.objects;
 
+import comn.functions.Database;
 import comn.interfaces.slogan;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 
 public class HotelResort extends Hotel implements slogan {
     private Integer id;
+
+    public HotelResort(Integer id) throws SQLException, IOException {
+        this.id = id;
+
+        if (id != null && Database.getConnection() != null) {
+            String sql = "SELECT * FROM hotelResort WHERE id = " + id;
+
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+
+                if (resultSet.next()) {
+                    // Retrieve data from the result set and assign it to the class properties
+                    this.id = resultSet.getInt("id");
+                    // Assign other properties as needed
+                }
+
+                resultSet.close();
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void store() {
+        if (id == null) {
+            id = Database.getNextIncrement("hotelResort");
+        }
+
+        String sql = "INSERT INTO hotelResort (id) VALUES (" + id + ") ON DUPLICATE KEY UPDATE id = " + id;
+
+        try {
+            Statement statement = Database.getConnection().createStatement();
+            statement.executeUpdate(sql);
+            statement.close();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remove() {
+        if (id != null) {
+            String sql = "DELETE FROM hotelResort WHERE id = " + id;
+
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                statement.executeUpdate(sql);
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static int find(Integer id) {
+        String sql = "SELECT id FROM hotelResort WHERE id = " + id;
+
+        try {
+            Statement statement = Database.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            int count = resultSet.next() ? 1 : 0;
+            resultSet.close();
+            statement.close();
+            return count;
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static void remover(Integer id) {
+        if (id != null) {
+            String sql = "DELETE FROM hotelResort WHERE id = " + id;
+
+            try {
+                Statement statement = Database.getConnection().createStatement();
+                statement.executeUpdate(sql);
+                statement.close();
+            } catch (SQLException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public HotelResort() {
         super();
     }
